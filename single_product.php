dad94b2ba3f4c5f6cae554215b3514059ac6a4f6
leoAshu <ashutosh.ojha2009@gmail.com>
2022-11-04 13:39:03 -0700

    adds product id value to add to cart form

--- single_product.php
+++ single_product.php
@@ -105,13 +105,14 @@ if(isset($_GET['product_id'])) {
                     <h2>$<?php echo $row['product_price']; ?></h2>
 
                     <form method="POST" action="cart.php">
+                        <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>"/>
                         <input type="hidden" name="product_image" value="<?php echo $row['product_image']; ?>"/>
                         <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>"/>
                         <input type="hidden" name="product_price" value="<?php echo $row['product_price']; ?>"/>
                         <input type="number" name="product_quantity" value="1"/>
                         <button class="buy-btn" type="submit" name="add_to_cart">Add To Cart</button>
                     </form>
-                    
+
                     <h4 class="mt-5 mb-5">Product details</h4>
                     <span><?php echo $row['product_description']; ?></span>
                 </div>
